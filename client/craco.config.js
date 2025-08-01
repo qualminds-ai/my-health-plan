05eefcc0bbb4563f35ac4d673aa0c49f25b67e0a
Sateesh <sateesh@qualminds.com>
2025-08-01 18:13:59 +0530

    feat: Implement cache busting in webpack configuration and add cache control meta tags in HTML

--- client/craco.config.js
+++ client/craco.config.js
@@ -7,4 +7,26 @@ module.exports = {
             ],
         },
     },
+    webpack: {
+        configure: (webpackConfig, { env }) => {
+            // Add cache busting for production builds
+            if (env === 'production') {
+                // Ensure unique filenames for cache busting
+                webpackConfig.output.filename = 'static/js/[name].[contenthash:8].js';
+                webpackConfig.output.chunkFilename = 'static/js/[name].[contenthash:8].chunk.js';
+
+                // Configure asset filenames with content hash
+                const oneOf = webpackConfig.module.rules.find(rule => rule.oneOf);
+                if (oneOf) {
+                    const assetRule = oneOf.oneOf.find(rule => rule.type === 'asset');
+                    if (assetRule) {
+                        assetRule.generator = {
+                            filename: 'static/media/[name].[contenthash:8][ext]'
+                        };
+                    }
+                }
+            }
+            return webpackConfig;
+        }
+    }
 }
